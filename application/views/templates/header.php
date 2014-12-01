8c1aaf90049e1ba18402b934e26a3539ae10cac2
chusnen <chusnen@hotmail.com>
2014-12-01 09:27:25 +0100

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,5 @@
+<html>
+<head>
+<title><?php echo $title ?>GestProf</title>
+<link href="css/style.css" rel="stylesheet" type="text/css"  />
+</head>
---
+++ application/views/templates/navigator.php
@@ -0,0 +1,9 @@
+<body>
+<nav>
+		<ul>
+			<li><a href="<?php echo base_url(); ?>index.php/tutorial/inicio">Inicio</a><li>
+			<li><a href="<?php echo base_url(); ?>index.php/tutorial/ingresos">Ingresos</a><li>
+			<li><a href="<?php echo base_url(); ?>index.php/tutorial/gastos">Gastos</a><li>
+			<li><a href="<?php echo base_url(); ?>index.php/tutorial/analisis">Analisis</a><li>
+		</ul>
+</nav>							
